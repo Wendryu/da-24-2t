defdc204ca38be9af0d8a1f50725a070d10b1b42
Pedro Ferreira <wendryuluan@gmail.com>
2024-08-15 14:25:05 -0300

    trabalho

---
+++ aulas/aula3/index.js
@@ -0,0 +1,59 @@
+// texto exemplo
+
+/* texto comentado
+linha 1 
+linha 2
+linha 3 */
+
+console.log("ola mundo")
+
+console.log("é o gremio e não adianta")
+
+prompt("qual é seu nome ?")
+
+const nome = "ruan da fazer 250"
+const idade = 18 
+
+console.log("ola", "meu nome é:", nome, "e eu tenho:", idade, "anos");
+
+const sobrenome = "ruan cocota de matos"
+const estudante = "no ensino medio"
+
+console.log("ola", "meu nome é:", sobrenome, "e eu to estudando:", estudante, "no cristo rei");
+
+const got = "stranger things"
+const temporadasDeGot = "4";
+
+console.log(typeof got)
+console.log(typeof temporadasDeGot)
+
+let novaVariavel
+console.log(typeof novaVariavel)
+
+novaVariavel = 4
+console.log(typeof novaVariavel)
+
+novaVariavel = undefined
+console.log(typeof novaVariavel)
+
+const novonome = prompt("teu nome mano veio ?");
+console.log( novonome)
+
+const novaidade = prompt("qual sua idade ?");
+console.log(novaidade)
+
+console.log(typeof novonome)
+console.log(typeof novaidade)
+
+const idadeNUmero = 77
+const idadeTexto = idadeNumero
+console.log(typeof idadeNumero)
+console.log(typeof idadeTexto)
+
+const idadeTexto2 = "77"
+const idadeNumero2 = number(idadeTexto2)
+console.log(typeof idadeTexto2)
+console.log(typeof idadeNumero2)
+
+git
+push
---
+++ aulas/aula3/trabalho av/index.js
@@ -0,0 +1,20 @@
+ //trabalho avaliativo
+ /*
+Celsius     fahrenheit      respostas do programa
+24              75.2                 75.2
+13              55.4                 55.4
+-2              28,4                 28,4
+-21             -5,8                 -5,8
+
+conclui a tarefa sem erro
+
+ */
+
+ let celsius
+let fahrenheit
+
+celsius = prompt("qual é o grau celsius?") 
+console.log("numero em celsius é",celsius)
+fahrenheit = (celsius *1.8) + 32
+console.log("Em fahrenheit fica igual a",fahrenheit)
+
