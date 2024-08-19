a4a8f3f32632faa2c1c22dc6c0548de8c6042b7d
Pedro Ferreira <wendryuluan@gmail.com>
2024-08-19 17:10:39 -0300

    aulas

---
+++ aula6/index.js
@@ -0,0 +1,116 @@
+//aula6 operadores aritimeticos
+/*
+const primeirovalor = 10
+const segundovalor = 20
+
+const resultado = primeirovalor + segundovalor + 4
+
+console.log("o resultado é:", resultado)
+
+*/
+/*
+const primeirovalor = 10
+const segundovalor = 20
+const resultado = primeirovalor - segundovalor
+
+console.log("o resultado é", resultado)
+
+*/
+/*
+const primeirovalor = 10
+const segundovalor = 20
+const resultado = primeirovalor * segundovalor
+
+console.log("o resultado é", resultado)
+*/
+/*
+const primeirovalor = 345
+const segundovalor = 10
+
+const resultado = primeirovalor / segundovalor
+
+console.log("o resultado é", resultado)
+*/
+
+//exemplo resto da divisão
+/*
+const restoDivisão = 11%4
+
+console.log("o resultado é:", restoDivisão)
+*/
+/*
+const primeirovalor = 3
+const segundovalor = 4
+
+const resultado = primeirovalor + segundovalor + 4
+
+console.log("o resultado é:", resultado)
+*/
+/*
+const primeirovalor = 3
+const segundovalor = 5
+const resultado = primeirovalor * segundovalor / 2 
+
+console.log("o resultado é", resultado)
+*/
+/*
+const primeirovalor = 5
+const segundovalor = 4
+const resultado = primeirovalor - segundovalor * -1
+
+console.log("o resultado é", resultado)
+*/
+/*
+const primeirovalor = 234
+const segundovalor = 5
+
+const resultado = primeirovalor / segundovalor
+
+console.log("o resultado é", resultado)
+*/
+/*
+const restoDivisão = 234%5
+
+console.log("o resultado é:", restoDivisão)
+*/
+
+//exemplos comparadores
+//comparador igual
+
+/*
+const condicão = 1 === 2
+console.log(condição)//resultado true
+//comparação do valor e tipo igual
+const condicão2 = 1 === "1"
+console.log(condição2)//resultado false
+
+//comparador diferente
+
+const condição = 1  !== "1"
+console.log(condição)//resultado true
+const condição2 = 1 !== 1 
+console.log(condição2)//resultado false
+
+const condição3 = 2 !== 1
+console.log(condição3)//resultado true
+*/
+
+//comparador maior, maior e igual
+/*
+const condicão = 1 > 2
+console.log(condicão)//resultado false
+const condicão1 = 2 > 2
+console.log(condicão1)//resultado false
+const condicão2 = 2 >= 2
+console.log(condicão2)//resultado true
+const condição3 = 3 > 2
+console.log(condição3)//resultado true
+*/
+
+const numero1 = 10
+const numero2 = 5
+
+console.log("o primeiro mumero é igual ao segundo ?", numero1 == numero2)//false
+console.log("o primeiro numero é diferente do segundo ?", numero1 == numero2)//true
+console.log("o primeiro numero é maior que o segundo ?", numero1 == numero2)//true
+console.log("o primeiro numero é menor que o segundo ?")//false
